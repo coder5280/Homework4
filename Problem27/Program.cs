bfde337b1ebddc692bb6bd9fef398a2bcf61d714
azamat Khadikov <khadikovazamat@gmail.com>
2023-02-26 22:20:46 -0700

    Problem 27

---
+++ Problem27/Program.cs
@@ -0,0 +1,16 @@
+﻿void Main(string[] args)
+{
+    int n, m; 
+    int sum = 0;
+    Console.Write("Enter a number: ");
+    n = Convert.ToInt32(Console.ReadLine());
+    while (n > 0)
+    {
+        m = n % 10;
+        sum = sum + m;
+        n = n / 10;
+    }
+    Console.Write("Sum is = " + sum);
+}  
+Main(args);
+

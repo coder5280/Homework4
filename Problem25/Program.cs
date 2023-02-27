3d5351ea3ee04207a033d45057155607f787a9c1
azamat Khadikov <khadikovazamat@gmail.com>
2023-02-26 21:40:57 -0700

    Problem25

---
+++ Problem25/Program.cs
@@ -0,0 +1,19 @@
+﻿int Pow(int numberA, int numberB)
+{
+    int result = 1;
+    for (int i = 1; i <= numberB; i++)
+    {
+        result = result * numberA;
+    }
+    return result;
+}
+
+//int numberA = 3;
+//int numberB = 5;
+Console.Write("Enter A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int res = Pow(numberA, numberB);
+Console.WriteLine("Answer " + res);

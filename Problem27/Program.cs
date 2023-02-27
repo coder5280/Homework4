c57dc5d3f77390c0ade717b9f41b2b2063a5451b
azamat Khadikov <khadikovazamat@gmail.com>
2023-02-26 22:24:07 -0700

    Problem 27updated

--- Problem27/Program.cs
+++ Problem27/Program.cs
@@ -4,7 +4,7 @@
     int sum = 0;
     Console.Write("Enter a number: ");
     n = Convert.ToInt32(Console.ReadLine());
-    while (n > 0)
+    while (n > 0 || n < 0)
     {
         m = n % 10;
         sum = sum + m;
